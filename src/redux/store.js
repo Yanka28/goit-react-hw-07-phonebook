eeeed28beff85f8551a334fb83689b4e9e4bcd42
Yanka28 <yanachepeluk@ukr.net>
2023-10-10 09:00:18 +0300

    com4

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,33 +8,3 @@ export const store = configureStore({
     filter: filterReducer,
   },
 });
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import { persistedContact } from './contactsSlice';
-// import { filterReducer } from './filterSlice';
-
-// import persistStore from 'redux-persist/es/persistStore';
-
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persistedContact,
-//     filter: filterReducer,
-//   },
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
